8e0fa9733ea13889770d07a9ff89025b3b060f9a
jorge sayago <130063995+JorgeSayago@users.noreply.github.com>
2025-03-31 14:33:05 -0500

    Commit

---
+++ src/Topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+// en typescript tenemos errores en tiempo de escritura
+const name: String= 'Jorge';
+let hpPoints: number | "FULL" = 95;
+const isAlive: boolean ="true";
+// en typescript tenemos errores en tiempo de escritura
+
+hpPoints= 'FULL';
+console.log({
+    name, hpPoints, isAlive
+}
+
+)
+export{};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-
+// import './Topics/01-basic-types';
 // npm run dev para q se ejecute en la consola en modo de desarrollo
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
